2f67488434db639179a5a6549ae223729b29ebd4
RoryLetteney <rory.letteney@gmail.com>
2019-03-25 16:52:36 -0500

    Built product name formatter, random number generator, productPicker, createElements function, and render the products to the page.

--- app.js
+++ app.js
@@ -15,26 +15,16 @@ var allProducts = [
   new Product('img/pet-sweep.jpg'),
   new Product('img/scissors.jpg'),
   new Product('img/shark.jpg'),
-  new Product('img/sweep.jpg'),
+  new Product('img/sweep.png'),
   new Product('img/tauntaun.jpg'),
   new Product('img/unicorn.jpg'),
   new Product('img/usb.gif'),
   new Product('img/water-can.jpg'),
   new Product('img/wine-glass.jpg')
 ];
-var previousImages = [];
+var previousProducts = [];
 var totalClicks = 0;
-
-function formatProductName(string) {
-  var stringManipulation = '';
-  var result = [];
-  stringManipulation = string.slice((string.indexOf('/') + 1), string.indexOf('.'));
-  stringManipulation = stringManipulation.split('-');
-  stringManipulation.forEach(function(word) {
-    result.push(word.charAt(0).toUpperCase() + word.slice(1));
-  });
-  return result.join(' ');
-}
+var numProdToShow = 3;
 
 function Product(imagePath) {
   this.name = formatProductName(imagePath);
@@ -49,3 +39,53 @@ function Product(imagePath) {
   };
 }
 
+function formatProductName(string) {
+  var stringManipulation = '';
+  var result = [];
+  stringManipulation = string.slice((string.indexOf('/') + 1), string.indexOf('.'));
+  stringManipulation = stringManipulation.split('-');
+  stringManipulation.forEach(function(word) {
+    result.push(word.charAt(0).toUpperCase() + word.slice(1));
+  });
+  return result.join(' ');
+}
+
+function randomProductIndex() {
+  return Math.floor(Math.random() * allProducts.length);
+}
+
+function pickProducts() {
+  var results = [];
+  var i = 0;
+  while (i < numProdToShow) {
+    var randomProduct = randomProductIndex();
+    if (!previousProducts.includes(allProducts[randomProduct])) {
+      previousProducts.push(allProducts[randomProduct]);
+      results.push(allProducts[randomProduct]);
+      i++;
+    }
+  }
+  previousProducts = results;
+  return results;
+}
+
+function createElements(tag, classes, parent, src) {
+  var elToCreate = document.createElement(tag);
+  classes.forEach(function(name) {
+    elToCreate.classList.add(name);
+  });
+  if (src) {
+    elToCreate.setAttribute('src', src);
+  }
+  parent.appendChild(elToCreate.cloneNode(true));
+}
+
+function showProducts() {
+  var productsSection = document.getElementById('products');
+  var currentProducts = pickProducts();
+  currentProducts.forEach(function(product) {
+    createElements('img', ['products__image'], productsSection, product.imagePath);
+  });
+}
+
+showProducts();
